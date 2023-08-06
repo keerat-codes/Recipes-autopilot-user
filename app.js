f1d490513e1182797256af2f14ff9a0dccd55dcf
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-07 00:56:25 +0530

    Updated App.js for recipes route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const recipesRoute = require('./routes/recipes');
+app.use('/recipes', recipesRoute);
 
 const conn = require('./middlewares/connection');
 console.log("Trying to establish a connection to TiDB...");
